b31badb3c33c36b221f373afd316073c64a367ae
atuobinnaa <161654854+atuobinnaa@users.noreply.github.com>
2024-02-29 00:27:15 +0100

    first commit

---
+++ timer.js
@@ -0,0 +1,25 @@
+// Set the countdown date to February 29, 2024, at midnight
+var countDownDate = new Date("Feb 29, 2024 09:30:00").getTime();
+
+// Update the countdown every millisecond
+var x = setInterval(function() {
+    // Get the current time
+    var now = new Date().getTime();
+
+    // Calculate the remaining time until the countdown date
+    var distance = countDownDate - now;
+
+    // Calculate days, hours, minutes and seconds remaining
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    // Update the HTML elements displaying days, hours, minutes and seconds
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}, 1);
